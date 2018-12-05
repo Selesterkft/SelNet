355c0589159d1c722779da357f292f16c62830b8
Mészáros Attila Gábor <meszarosag@gmail.com>
2018-12-05 22:21:58 +0100

    Login design

--- app/src/main/java/hu/selester/seltransport/Fragments/LoginFragment.kt
+++ app/src/main/java/hu/selester/seltransport/Fragments/LoginFragment.kt
@@ -1,47 +1,33 @@
 package hu.selester.seltransport.Fragments
 
-import android.content.Context
+import android.content.res.Resources
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.JsonObjectRequest
 import hu.selester.seltransport.Adapters.LoginAdapter
 import hu.selester.seltransport.BuildConfig
-import hu.selester.seltransport.Database.SelTransportDatabase
-import hu.selester.seltransport.Database.Tables.PhotosTable
-import hu.selester.seltransport.Helper.HelperClass
-import hu.selester.seltransport.Helper.MySingleton
-import hu.selester.seltransport.Objects.SessionClass
 import hu.selester.seltransport.R
-import kotlinx.android.synthetic.main.frg_login.*
 import kotlinx.android.synthetic.main.frg_login.view.*
-import org.json.JSONArray
-import org.json.JSONObject
-import java.lang.Exception
-import java.sql.Date
-import java.text.SimpleDateFormat
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.*
-import kotlin.concurrent.thread
+
 
 class LoginFragment : Fragment(){
 
     lateinit var rootView: View
+
+    fun pxToDp(num:Float):Int{
+        return (num * Resources.getSystem().displayMetrics.density).toInt()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         rootView = LayoutInflater.from(this.context).inflate(R.layout.frg_login, container, false)
         rootView.login_version.text = "Verzió: " + BuildConfig.VERSION_NAME
         rootView.login_tabLayout.addTab(rootView.login_tabLayout.newTab().setIcon(R.drawable.tab_qr))
         rootView.login_tabLayout.addTab(rootView.login_tabLayout.newTab().setIcon(R.drawable.tab_login))
-
         rootView.login_viewpager.adapter = LoginAdapter(childFragmentManager)
         rootView.login_viewpager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(rootView.login_tabLayout))
         rootView.login_tabLayout.addOnTabSelectedListener(object :
@@ -56,6 +42,19 @@ class LoginFragment : Fragment(){
 
         })
         rootView.login_viewpager.currentItem = 0
+        rootView.login_viewpager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(p0: Int) {
+            }
+
+            override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
+                if(p1 > 0.0 && p1 < 1.0)
+                    rootView.login_backgroundPic.scrollX = pxToDp(  -100 + (200*p1)  )
+
+            }
+
+            override fun onPageSelected(p0: Int) {
+            }
+        })
         return rootView
     }
 
